946529c9fa9547ec40eb83ce92da0a604ac7500b
matt-janisz <mjanisz@gmail.com>
2014-01-26 09:03:26 -0500

    Updated comments and refactored sum_divisors.
    Comments updated to jsdoc convention.
    Refactor sum_divisors for readability.

--- projecteuler/p0001/multiples_of_3_or_5.js
+++ projecteuler/p0001/multiples_of_3_or_5.js
@@ -1,29 +1,34 @@
 'use strict';
 
-// param a is the first term
-// param d is the "common difference" or step between numbers
-// param n is the number of terms to sum
+/**
+ * Calculates the arithmetic sum of a sequence according to:
+ * http://www.mathsisfun.com/algebra/sequences-sums-arithmetic.html
+ * 
+ * @param {number} a The first term
+ * @param {number} d The "common difference" or step between numbers in the sequence
+ * @param {number} n The number of terms in the sequence to sum
+ * @return {number} The arithmetic sum of the sequence defined by a, d, and n
+ */
 const arithmetic_sum = function(a, d, n) {
   return (n / 2) * (2 * a + d * (n - 1));
 };
 
-// param a defines the top of the range to sum (1, a)
-// param b is the first divisor
-// param c is the second divisor
-const sum_divisors = function(a, b, c) {
-  var bc = b * c;
-  // Get the count of b, c, and b*c multiples in (1, a)
-  var count_b = Math.floor((a - 1) / b);
-  var count_c = Math.floor((a - 1) / c);
-  var count_bc = Math.floor((a - 1) / bc);
+/**
+ * Calculates the sum of the divisors {b, c} in range (1, upper)
+ * @param {number} upper The upperbound of the range to sum (1, upper)
+ * @param {number} div1 The first divisor
+ * @param {number} div2 The second divisor
+ * @return {number} The sum of the multiples
+ */
+const sum_divisors = function(upper, div1, div2) {
+  // The range is exclusive, so subtract 1 to get the inclusive upperbound
+  const upper_inclusive = upper - 1;
+  // Need the combination of div1 * div2 to subtract the double-counting of common mulitples
+  const common_div = div1 * div2;
 
-  // Get the sum of b, c, and b*c
-  var sum = arithmetic_sum(b, b, count_b);
-  sum += arithmetic_sum(c, c, count_c);
-  // Subtract b*c since we've double-counted it
-  sum -= arithmetic_sum(bc, bc, count_bc);
-
-  return sum;
+  return arithmetic_sum(div1, div1, Math.floor((upper_inclusive) / div1)) +
+      arithmetic_sum(div2, div2, Math.floor((upper_inclusive) / div2)) -
+      arithmetic_sum(common_div, common_div, Math.floor((upper_inclusive) / common_div));
 };
 
 exports.sum_divisors = sum_divisors;
