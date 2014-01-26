d25def43a8d46282d941b2a189d85b759b02a68f
matt-janisz <mjanisz@gmail.com>
2014-01-26 09:12:48 -0500

    Fixed lint formatting errors

--- projecteuler/p0001/multiples_of_3_or_5.js
+++ projecteuler/p0001/multiples_of_3_or_5.js
@@ -1,17 +1,19 @@
 'use strict';
 
+
 /**
  * Calculates the arithmetic sum of a sequence according to:
  * http://www.mathsisfun.com/algebra/sequences-sums-arithmetic.html
- * 
+ *
  * @param {number} a The first term
- * @param {number} d The "common difference" or step between numbers in the sequence
+ * @param {number} d The "common difference" or step between numbers
  * @param {number} n The number of terms in the sequence to sum
  * @return {number} The arithmetic sum of the sequence defined by a, d, and n
  */
 const arithmetic_sum = function(a, d, n) {
   return (n / 2) * (2 * a + d * (n - 1));
 };
+
 
 /**
  * Calculates the sum of the divisors {b, c} in range (1, upper)
@@ -23,7 +25,7 @@ const arithmetic_sum = function(a, d, n) {
 const sum_divisors = function(upper, div1, div2) {
   // The range is exclusive, so subtract 1 to get the inclusive upperbound
   const upper_inclusive = upper - 1;
-  // Need the combination of div1 * div2 to subtract the double-counting of common mulitples
+  // Need the combo of div1 * div2 to subtract divisor double-counts
   const common_div = div1 * div2;
 
   return arithmetic_sum(div1, div1, Math.floor((upper_inclusive) / div1)) +
