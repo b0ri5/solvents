f6c271650d80714d4a534806cb68130dc3745eed
matt-janisz <mjanisz@gmail.com>
2014-01-25 19:50:28 -0500

    Formatting for lint

--- projecteuler/p0001/multiples_of_3_or_5.js
+++ projecteuler/p0001/multiples_of_3_or_5.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // param a is the first term
 // param r is the "common ratio" or step between numbers
@@ -11,18 +11,18 @@ const geometric_sum = function(a, r, n) {
 // param b is the first divisor
 // param c is the second divisor
 const sum_divisors = function(a, b, c) {
-  var bc = b*c;
+  var bc = b * c;
   // Get the count of b, c, and b*c multiples in (1, a)
   var count_b = Math.floor((a - 1) / b);
   var count_c = Math.floor((a - 1) / c);
   var count_bc = Math.floor((a - 1) / bc);
-  
+
   // Get the sum of b, c, and b*c
   sum = geometric_sum(b, b, count_b);
   sum += geometric_sum(c, c, count_c);
   // Subtract b*c since we've double-counted it
   sum -= geometric_sum(bc, bc, count_bc);
-  
+
   return sum;
 };
 
