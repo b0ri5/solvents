2c7bae77049aefd5641a65d7da8c65f7fb9fda84
matt-janisz <mjanisz@gmail.com>
2014-01-25 19:38:18 -0500

    Initial commit

---
+++ projecteuler/p0001/multiples_of_3_or_5_test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('assert');
+const multiples_of_3_or_5 = require('./multiples_of_3_or_5');
+
+describe('multiples_of_3_or_5', function() {
+  describe('#sum_divisors()', function() {
+    it('should be exclusive', function() {
+      assert.equal(0, multiples_of_3_or_5.sum_divisors(3));
+      assert.equal(3, multiples_of_3_or_5.sum_divisors(4));
+      assert.equal(3, multiples_of_3_or_5.sum_divisors(5));
+      assert.equal(8, multiples_of_3_or_5.sum_divisors(6));
+    });
+    it('should solve small example', function() {
+      assert.equal(23, multiples_of_3_or_5.sum_divisors(10));
+    });
+    it('should solve large problem', function() {
+      assert.equal(233168, multiples_of_3_or_5.sum_divisors(1000));
+    });
+  });
+});
