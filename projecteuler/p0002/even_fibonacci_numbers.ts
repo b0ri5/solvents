52453d636f63af1763a90ec51d1cb9ecd40e7f1b
b0ri5 <borisb0ri5@gmail.com>
2020-08-18 11:58:50 -0700

    Use destructuring for even_fibonacci_numbers
    See https://www.typescriptlang.org/docs/handbook/variable-declarations.html#destructuring for more info on destructuring

--- projecteuler/p0002/even_fibonacci_numbers.ts
+++ projecteuler/p0002/even_fibonacci_numbers.ts
@@ -1,21 +1,17 @@
 export const bruteForce = function (n: number) {
-  let a = 1;
-  let b = 2;
+  let [a, b] = [1, 2];
   let sum = 0;
   do {
     if (b % 2 === 0) {
       sum += b;
     }
-    const tmp = b;
-    b = a + b;
-    a = tmp;
+    [a, b] = [b, a + b];
   } while (b < n);
   return sum;
 };
 
 export const everyThird = function (n: number) {
-  let a = 1;
-  let b = 2;
+  let [a, b] = [1, 2];
   let sum = 0;
   do {
     // Every third fibonacci number is even starting with 2
@@ -31,9 +27,7 @@ export const everyThird = function (n: number) {
     //         = (2f_n + f_{n-1}) + (f_n + f_{n-1})
     //         = 3f_n + 2f_{n-1}
     sum += b;
-    const tmp = b;
-    b = 3 * b + 2 * a;
-    a = 2 * tmp + a;
+    [a, b] = [2 * b + a, 3 * b + 2 * a];
   } while (b < n);
   return sum;
 };
