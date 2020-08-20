09620dd2e1eba1c909002f85f5b2eacfa1d3b56a
Greg Tener <borisb0ri5@gmail.com>
2020-08-20 13:50:18 -0700

    Remove some unnecessary outer describes

--- projecteuler/p0002/even_fibonacci_numbers_test.ts
+++ projecteuler/p0002/even_fibonacci_numbers_test.ts
@@ -1,19 +1,17 @@
 import {bruteForce, everyThird} from './even_fibonacci_numbers';
 
-describe('even_fibonacci_numbers', () => {
-  const describeSolution = function (
-    name: string,
-    solution: (n: number) => number
-  ) {
-    describe(name, () => {
-      it('should solve small input', () => {
-        expect(solution(89)).toBe(44);
-      });
-      it('should solve large input', () => {
-        expect(solution(4000000)).toBe(4613732);
-      });
+const describeSolution = function (
+  name: string,
+  solution: (n: number) => number
+) {
+  describe(name, () => {
+    it('should solve small input', () => {
+      expect(solution(89)).toBe(44);
     });
-  };
-  describeSolution('bruteForce', bruteForce);
-  describeSolution('everyThird', everyThird);
-});
+    it('should solve large input', () => {
+      expect(solution(4000000)).toBe(4613732);
+    });
+  });
+};
+describeSolution('bruteForce', bruteForce);
+describeSolution('everyThird', everyThird);
