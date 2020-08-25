6890e70b2bda4e786650b9156788dec1738e520a
Greg Tener <borisb0ri5@gmail.com>
2020-08-24 17:33:07 -0700

    Use a map instead of an object for nthPrime

--- projecteuler/p0007/10001st_prime_test.ts
+++ projecteuler/p0007/10001st_prime_test.ts
@@ -5,5 +5,6 @@ describe('nthPrime', () => {
     expect(nthPrime(1)).toBe(2);
     expect(nthPrime(6)).toBe(13);
     expect(nthPrime(10001)).toBe(104743);
+    expect(nthPrime(1000000)).toBe(15485863);
   });
 });
