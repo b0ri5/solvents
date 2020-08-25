94a7ca60810a8bb8642962b70f9703a63ee4cab2
Greg Tener <borisb0ri5@gmail.com>
2020-08-25 16:59:59 -0700

    .

--- projecteuler/p0007/10001st_prime.ts
+++ projecteuler/p0007/10001st_prime.ts
@@ -1,32 +1,100 @@
-// This does an unbounded sieve
-export const nthPrime = function (n: number): number {
-  // A map from composite numbers to the prime factors that resulted in them being known-composite
+// Probably the simplest unbounded sieve
+export const simplestPrimes = function* () {
   const composites = new Map<number, number[]>();
-  const addComposite = function (composite: number, prime: number): void {
-    let primes = composites.get(composite);
-    if (!primes) {
-      primes = [];
-      composites.set(composite, primes);
+  const add = function (pq: number, p: number) {
+    let ps = composites.get(pq);
+    if (!ps) {
+      ps = [];
+      composites.set(pq, ps);
     }
-    primes.push(prime);
+    ps.push(p);
   };
-  let latestPrime = 2;
-  let numPrimes = 1;
-  for (let i = 3; numPrimes < n; i += 2) {
-    const primes = composites.get(i);
-    if (primes) {
-      for (const p of primes) {
-        // We are skipping evens so ignore i + 2p next
-        addComposite(i + 2 * p, p);
+  for (let i = 2; ; i++) {
+    const ps = composites.get(i);
+    if (ps) {
+      for (const p of ps) {
+        add(i + p, p);
       }
       composites.delete(i);
     } else {
-      numPrimes += 1;
-      latestPrime = i;
-      // The next multiple to check should not be divisible by
-      // any smaller prime so check i*i next
-      addComposite(i * i, i);
+      yield i;
+      add(2 * i, i);
     }
   }
-  return latestPrime;
 };
+
+// Like simplestPrimes but uses a step size of two and initially
+// adds the first composite to check as the square of the prime.
+export const simplerPrimes = function* () {
+  const composites = new Map<number, number[]>();
+  const add = function (pq: number, p: number) {
+    let ps = composites.get(pq);
+    if (!ps) {
+      ps = [];
+      composites.set(pq, ps);
+    }
+    ps.push(p);
+  };
+  yield 2;
+  for (let i = 3; ; i += 2) {
+    const ps = composites.get(i);
+    if (ps) {
+      for (const p of ps) {
+        add(i + 2 * p, p);
+      }
+      composites.delete(i);
+    } else {
+      yield i;
+      add(i * i, i);
+    }
+  }
+};
+
+// Like simplerPrimes but when re-adding a composite for a prime,
+// the composite will not be divisible by any smaller primes.
+//
+// The big difference here is that the map is from composite to a
+// generator of the next composites to use.
+export const primes = function* () {
+  const composites = new Map<number, Generator<number>>();
+  yield 2;
+  const allPrimes = [];
+  for (let i = 3; ; i += 2) {
+    let gen = composites.get(i);
+    if (gen) {
+      composites.delete(i);
+    } else {
+      yield i;
+      allPrimes.push(i);
+      gen = multiplesOfPrimeNotDivisibleBySmallerPrimes(allPrimes, i);
+    }
+    composites.set(gen.next().value, gen);
+  }
+};
+
+
+// This is a generator for all numbers which are multiples
+// of a given prime but not divisible by smaller primes.
+// For example, for 7 this will generate all numbers like
+// 2^0 * 3^0 * 5^0 * 7n where n >= 7.
+export function* multiplesOfPrimeNotDivisibleBySmallerPrimes(
+  primes: number[],
+  p: number
+) {
+  yield p * p;
+  for (let i = p + 2; ; i += 2) {
+    let isDivisibleBySmallerPrime = false;
+    for (const q of primes) {
+      if (q === p) {
+        break;
+      }
+      if (i % q === 0) {
+        isDivisibleBySmallerPrime = true;
+        break;
+      }
+    }
+    if (!isDivisibleBySmallerPrime) {
+      yield p * i;
+    }
+  }
+}
