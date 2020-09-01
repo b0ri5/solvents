6bdc913c63235237c88150574f654444be13f66e
Greg Tener <borisb0ri5@gmail.com>
2020-09-01 11:27:38 -0700

    .

--- projecteuler/p0007/benchmark.ts
+++ projecteuler/p0007/benchmark.ts
@@ -1,12 +1,10 @@
-import {add, complete, cycle, suite} from 'benny';
+import {add, complete, cycle, save, suite} from 'benny';
 import {primes, simplerPrimes, simplestPrimes} from './10001st_prime';
 
-const firstN = function (gen: Generator<number>, n: number) {
-  const a: number[] = [];
+const consumeN = function (gen: Generator<number>, n: number) {
   for (let i = 0; i < n; i++) {
-    a.push(gen.next().value);
+    gen.next().value;
   }
-  return a;
 };
 
 const options = {
@@ -14,29 +12,25 @@ const options = {
   maxTime: 30,
 };
 
+const primesBenchmarks = (...cases: [string, Generator<number>][]) =>
+  cases.map(([name, gen]) => {
+    return add(
+      name,
+      () => {
+        consumeN(gen, 2_000);
+      },
+      options
+    );
+  });
+
 suite(
   'sieves',
-  add(
-    'simplestPrimes',
-    () => {
-      firstN(simplestPrimes(), 20_000);
-    },
-    options
-  ),
-  add(
-    'simplerPrimes',
-    () => {
-      firstN(simplerPrimes(), 20_000);
-    },
-    options
-  ),
-  add.only(
-    'primes',
-    () => {
-      firstN(primes(), 500_000);
-    },
-    options
+  ...primesBenchmarks(
+    ['simplestPrimes', simplestPrimes()],
+    ['simplerPrimes', simplerPrimes()],
+    ['primes', primes()]
   ),
   cycle(),
-  complete()
+  complete(),
+  save({file: 'sieves', details: true})
 );
