4b33ee5a8d6130a5db8b7dd68ecbdb0c6fb8f875
Greg Tener <borisb0ri5@gmail.com>
2021-10-15 23:50:20 -0700

    Add a function for computing sieve gaps (#225)
    These a "black-key"-type gaps. The idea is that you can enumerate all integers not divisible by 2 or 3, then 2, 3, or 5, etc.
    
    The length of the gaps is https://oeis.org/A005867.

---
+++ fun/sieve/gaps.py
@@ -0,0 +1,21 @@
+import functools
+
+
+@functools.cache
+def gaps(prime_index):
+    if prime_index == 2:
+        return (3, (2,))
+    prev_prime, prev_gaps = gaps(prime_index - 1)
+    next_prime = prev_prime + prev_gaps[0]
+    # Rotate the previous gaps one to the right: [g1, g2, ...] -> [g2, ..., g1]
+    prev_gaps = prev_gaps[1:] + (prev_gaps[0],)
+    running_sum = next_prime
+    new_gaps = []
+    for gaps_index in range(prev_prime * len(prev_gaps)):
+        prev_gap = prev_gaps[gaps_index % len(prev_gaps)]
+        if running_sum % prev_prime == 0:
+            new_gaps[-1] += prev_gap
+        else:
+            new_gaps.append(prev_gap)
+        running_sum += prev_gap
+    return (next_prime, tuple(new_gaps))
