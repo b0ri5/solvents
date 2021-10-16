7828755979ef8c282f08a44c1c755fd25412050c
Greg Tener <borisb0ri5@gmail.com>
2021-10-15 23:44:25 -0700

    Make the linter happier

--- fun/sieve/gaps.py
+++ fun/sieve/gaps.py
@@ -9,13 +9,13 @@ def gaps(prime_index):
     next_prime = prev_prime + prev_gaps[0]
     # Rotate the previous gaps one to the right: [g1, g2, ...] -> [g2, ..., g1]
     prev_gaps = prev_gaps[1:] + (prev_gaps[0],)
-    sum = next_prime
+    running_sum = next_prime
     new_gaps = []
     for gaps_index in range(prev_prime * len(prev_gaps)):
         prev_gap = prev_gaps[gaps_index % len(prev_gaps)]
-        if sum % prev_prime == 0:
+        if running_sum % prev_prime == 0:
             new_gaps[-1] += prev_gap
         else:
             new_gaps.append(prev_gap)
-        sum += prev_gap
+        running_sum += prev_gap
     return (next_prime, tuple(new_gaps))
