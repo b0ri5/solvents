03096bc67a68525add70e61bbcb082ab7be2e889
Greg Tener <borisb0ri5@gmail.com>
2021-10-19 17:20:06 -0700

    .

--- fun/sieve/reduced_residue_system.py
+++ fun/sieve/reduced_residue_system.py
@@ -1,7 +1,8 @@
 # See https://en.wikipedia.org/wiki/Reduced_residue_system
 
+from functools import cache
 from math import gcd
-from sympy import primorial
+from sympy import prime, primorial
 
 
 def _reduced_residue_system_primorial_brute_force(i):
@@ -10,4 +11,20 @@ def _reduced_residue_system_primorial_brute_force(i):
     for k in range(1, primorial_i):
         if gcd(primorial_i, k) == 1:
             system.add(k)
-    return system
+    return frozenset(system)
+
+
+@cache
+def reduced_residue_system_primorial(i):
+    if i == 1:
+        return frozenset({1})
+    previous_rrs = reduced_residue_system_primorial(i - 1)
+    prime_i = prime(i)
+    previous_primorial = primorial(i - 1)
+    rrs = set()
+    for residue in previous_rrs:
+        for k in range(0, prime_i):
+            candidate = previous_primorial * k + residue
+            if candidate % prime_i != 0:
+                rrs.add(candidate)
+    return frozenset(rrs)
--- fun/sieve/reduced_residue_system_test.py
+++ fun/sieve/reduced_residue_system_test.py
@@ -1,9 +1,13 @@
 import unittest
 
-from reduced_residue_system import _reduced_residue_system_primorial_brute_force
+from math import gcd
+from reduced_residue_system import (
+    _reduced_residue_system_primorial_brute_force,
+    reduced_residue_system_primorial
+)
+from sympy import primorial
 
-
-class TestBruteForce(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def test_reduced_residue_system_primorial_brute_force(self):
         self.assertEqual({1}, _reduced_residue_system_primorial_brute_force(1))
@@ -26,6 +30,41 @@ class TestBruteForce(unittest.TestCase):
         }
         # See https://oeis.org/A005867
         self.assertEqual({1, 2, 8, 48, 480, 5760}, sizes)
+
+
+    def test_reduced_residue_system_primorial(self):
+        self.assertEqual({1}, reduced_residue_system_primorial(1))
+        self.assertEqual({1, 5},
+                         reduced_residue_system_primorial(2))
+        self.assertEqual({1, 7, 11, 13, 17, 19, 23, 29},
+                         reduced_residue_system_primorial(3))
+        self.assertEqual(
+            {
+                1, 131, 137, 11, 139, 13, 143, 17, 19, 149, 23, 151, 29, 157,
+                31, 163, 37, 167, 41, 169, 43, 173, 47, 179, 53, 181, 59, 187,
+                61, 191, 193, 67, 197, 71, 199, 73, 79, 209, 83, 89, 97, 101,
+                103, 107, 109, 113, 121, 127
+            }, reduced_residue_system_primorial(4))
+
+
+    def test_reduced_residue_system_primorial_sizes(self):
+        sizes = {
+            len(reduced_residue_system_primorial(i))
+            for i in range(1, 9)
+        }
+        # See https://oeis.org/A005867
+        self.assertEqual({1, 2, 8, 48, 480, 5760, 92160, 1658880}, sizes)
+
+
+    def test_reduced_residue_system_primorial_is_rrs(self):
+        for i in range(1, 8):
+            primorial_i = primorial(i)
+            rrs = reduced_residue_system_primorial(i)
+            for residue in rrs:
+                # Each residue must be less than primorial_i
+                self.assertLess(residue, primorial_i)
+                # And relatively prime to primorial_i
+                self.assertEqual(1, gcd(residue, primorial_i))
 
 
 if __name__ == '__main__':
