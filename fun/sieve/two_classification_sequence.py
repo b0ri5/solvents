6e9d2d9cce78c1105e6e843fb86c813e4d9daa40
Greg Tener <borisb0ri5@gmail.com>
2021-10-23 21:27:02 -0700

    Add a py_binary for the two-classification sequences (#246)
    These might be interesting to add to oeis and give some insight into the composition of twos.
    
    I was a bit surprised to see composite_to_composite seem to be the larger of the four.

---
+++ fun/sieve/two_classification_sequence.py
@@ -0,0 +1,8 @@
+from reduced_residue_system import reduced_residue_system_primorial_two_classification
+
+for i in range(1, 20):
+    two_classification = reduced_residue_system_primorial_two_classification(i)
+    print(i, two_classification.composite_to_composite,
+          two_classification.composite_to_prime,
+          two_classification.prime_to_composite,
+          two_classification.prime_to_prime, sum(two_classification))
