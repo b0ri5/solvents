5ee47af7a42e911f318ba9b3b09ed0c6139414b0
Greg Tener <borisb0ri5@gmail.com>
2021-11-12 23:13:41 -0800

    .

--- fun/sieve/max_num_factors_in_rrs_primorial.py
+++ fun/sieve/max_num_factors_in_rrs_primorial.py
@@ -9,14 +9,15 @@ def main():
         larger_prime = prime(i + 1)
         product = larger_prime
         num_factors = 0
-        while product < primorial(i):
+        primorial_value = primorial(i)
+        while product < primorial_value:
             num_factors += 1
             larger_prime = nextprime(larger_prime)
             product *= larger_prime
 
         all_num_factors.append(num_factors)
         all_products.append(product // larger_prime)
-        print(i, num_factors, primorial(i), product // larger_prime)
+        print(i, num_factors, primorial_value, product // larger_prime)
 
     print(all_num_factors)
     print(all_products)
