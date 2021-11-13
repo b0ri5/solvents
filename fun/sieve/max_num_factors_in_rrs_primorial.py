f565d7385bc49a6cf16643f55581051fe9ef3fdb
Greg Tener <borisb0ri5@gmail.com>
2021-11-12 23:19:27 -0800

    Add a script for outputting the maximum number of prime factors of a number in an rrs primorial system (#261)
    This doesn't seem to be in OEIS.
    
    This is potentially interesting because in trying to understand if min_prime_descendant always eventually returns, it could help to know the maximum number of factors.

---
+++ fun/sieve/max_num_factors_in_rrs_primorial.py
@@ -0,0 +1,26 @@
+from sympy import nextprime, prime, primorial
+
+
+def main():
+    all_num_factors = []
+    all_products = []
+    for i in range(1, 30):
+        larger_prime = prime(i + 1)
+        product = larger_prime
+        num_factors = 0
+        primorial_value = primorial(i)
+        while product < primorial_value:
+            num_factors += 1
+            larger_prime = nextprime(larger_prime)
+            product *= larger_prime
+
+        all_num_factors.append(num_factors)
+        all_products.append(product // larger_prime)
+        print(i, num_factors, primorial_value, product // larger_prime)
+
+    print(all_num_factors)
+    print(all_products)
+
+
+if __name__ == "__main__":
+    main()
