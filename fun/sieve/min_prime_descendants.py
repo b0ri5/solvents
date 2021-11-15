3f1cc199e267e5e5aacdc3ecf32ee094cc7c1daf
Greg Tener <borisb0ri5@gmail.com>
2021-11-14 23:35:31 -0800

    .

--- fun/sieve/min_prime_descendants.py
+++ fun/sieve/min_prime_descendants.py
@@ -1,16 +1,19 @@
-from itertools import count
-from reduced_residue_system import min_prime_descendant, reduced_residue_system_primorial_new
-from sympy import isprime
+from concurrent.futures import ProcessPoolExecutor
+from itertools import count, starmap
+from reduced_residue_system import min_prime_descendant, all_reduced_residue_system_primorial
+
+
+def star_min_prime_descendant(residue_i):
+    return min_prime_descendant(*residue_i)
 
 
 def main():
-    for i in count(start=2):
-        for residue in reduced_residue_system_primorial_new(i):
-            if isprime(residue):
-                continue
-            descendant, j = min_prime_descendant(residue, i)
-            if j - i > 1:
-                print(residue, i, descendant, j)
+    for (residue, i), (descendant, j) in zip(
+            all_reduced_residue_system_primorial(),
+            map(star_min_prime_descendant,
+                all_reduced_residue_system_primorial())):
+        if j - i > 1:
+            print(residue, i, descendant, j)
 
 
 if __name__ == '__main__':
