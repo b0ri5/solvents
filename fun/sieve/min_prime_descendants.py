48a7a5917d87761d95c7577c6b2189071992542c
Greg Tener <borisb0ri5@gmail.com>
2021-11-15 13:26:11 -0800

    Use multiple processors to run min_prime_descendants (#268)
    This was mostly just for fun, but it might also help find out if there is a within-reach example where a dept of 3 is needed to find a prime. I suspect https://bugs.python.org/issue29842 would make this a little bit easier.

--- fun/sieve/min_prime_descendants.py
+++ fun/sieve/min_prime_descendants.py
@@ -1,16 +1,47 @@
-from itertools import count
-from reduced_residue_system import min_prime_descendant, reduced_residue_system_primorial_new
-from sympy import isprime
+from concurrent.futures import ProcessPoolExecutor
+from itertools import islice
+from multiprocessing import Manager
+from reduced_residue_system import min_prime_descendant, all_reduced_residue_system_primorial
+
+
+def report_intersting_descendants(chunk, result_queue, completion_queue):
+    for residue, i in chunk:
+        descendant, j = min_prime_descendant(residue, i)
+        if j - i > 1:
+            result_queue.put((residue, i, descendant, j))
+    completion_queue.put(None)
 
 
 def main():
-    for i in count(start=2):
-        for residue in reduced_residue_system_primorial_new(i):
-            if isprime(residue):
-                continue
-            descendant, j = min_prime_descendant(residue, i)
-            if j - i > 1:
+    chunksize = 10000
+    max_outstanding = 100
+    manager = Manager()
+    result_queue = manager.Queue()
+    completion_queue = manager.Queue()
+    with ProcessPoolExecutor() as executor:
+        generator = all_reduced_residue_system_primorial()
+        num_outstanding = 0
+        while True:
+            # Drain the completion queue to get the number of outstanding processes.
+            while not completion_queue.empty():
+                completion_queue.get()
+                num_outstanding -= 1
+
+            # Wait until a process completes if the number of outstanding
+            # processes is at the maximum.
+            if num_outstanding == max_outstanding:
+                completion_queue.get()
+                num_outstanding -= 1
+
+            chunk = tuple(islice(generator, chunksize))
+            executor.submit(report_intersting_descendants, chunk, result_queue,
+                            completion_queue)
+            num_outstanding += 1
+            while not result_queue.empty():
+                residue, i, descendant, j = result_queue.get()
                 print(residue, i, descendant, j)
+                if j - i > 2:
+                    return
 
 
 if __name__ == '__main__':
