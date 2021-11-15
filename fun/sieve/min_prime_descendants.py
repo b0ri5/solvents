a9fdf5bcb1ed84103d1f4562de70d8fe60648408
Greg Tener <borisb0ri5@gmail.com>
2021-11-15 00:03:56 -0800

    .

--- fun/sieve/min_prime_descendants.py
+++ fun/sieve/min_prime_descendants.py
@@ -1,19 +1,26 @@
 from concurrent.futures import ProcessPoolExecutor
-from itertools import count, starmap
+from itertools import count, islice
+from multiprocessing import Queue
 from reduced_residue_system import min_prime_descendant, all_reduced_residue_system_primorial
 
 
-def star_min_prime_descendant(residue_i):
-    return min_prime_descendant(*residue_i)
+def report_intersting_descendants(chunk, queue):
+    for residue, i in chunk:
+        descendant, j = min_prime_descendant(residue, i)
+        if j - i > 1:
+            queue.put((residue, i, descendant, j))
 
 
 def main():
-    for (residue, i), (descendant, j) in zip(
-            all_reduced_residue_system_primorial(),
-            map(star_min_prime_descendant,
-                all_reduced_residue_system_primorial())):
-        if j - i > 1:
-            print(residue, i, descendant, j)
+    chunksize = 1000
+    queue = Queue()
+    with ProcessPoolExecutor() as executor:
+        generator = all_reduced_residue_system_primorial()
+        while True:
+            chunk = islice(generator, chunksize)
+            executor.submit(report_intersting_descendants, chunk, queue)
+            while not queue.empty():
+                print(*queue.get())
 
 
 if __name__ == '__main__':
