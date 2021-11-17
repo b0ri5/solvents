b97ae83682890c7d5befce2cd13d94378584eb28
Greg Tener <borisb0ri5@gmail.com>
2021-11-17 14:36:12 -0800

    .

--- fun/sieve/min_prime_descendants.py
+++ fun/sieve/min_prime_descendants.py
@@ -4,14 +4,14 @@ from concurrent.futures import ProcessPoolExecutor
 from itertools import islice, repeat
 from multiprocessing import Manager
 from reduced_residue_system import (all_reduced_residue_system_primorial,
-                                    min_prime_descendant,
+                                    min_prime_descendant, primoradic,
                                     reduced_residue_system_primorial_new)
 
 
 def report_intersting_descendants(chunk, result_queue, completion_queue):
     for residue, i in chunk:
         descendant, j = min_prime_descendant(residue, i)
-        if j - i > 1:
+        if j - i > 1 and primoradic(descendant)[-2] != 0:
             result_queue.put((residue, i, descendant, j))
     completion_queue.put(None)
 
@@ -43,14 +43,18 @@ def main():
                 num_outstanding -= 1
 
             chunk = tuple(islice(generator, chunksize))
-            executor.submit(report_intersting_descendants, chunk, result_queue,
-                            completion_queue)
-            num_outstanding += 1
+            if len(chunk) > 0:
+                executor.submit(report_intersting_descendants, chunk,
+                                result_queue, completion_queue)
+                num_outstanding += 1
             while not result_queue.empty():
                 residue, i, descendant, j = result_queue.get()
-                print(residue, i, descendant, j)
+                print(residue, i, descendant, j, primoradic(residue),
+                      primoradic(descendant))
                 if j - i > 2:
                     return
+            if num_outstanding == 0:
+                break
 
 
 if __name__ == '__main__':
