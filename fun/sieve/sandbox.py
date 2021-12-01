177b56c17ac35bea01d88507565673b5e10fa668
Greg Tener <borisb0ri5@gmail.com>
2021-11-30 23:32:25 -0800

    .

--- fun/sieve/sandbox.py
+++ fun/sieve/sandbox.py
@@ -1,7 +1,29 @@
-from reduced_residue_system import ancestors_compositeness, random_rrsp
+import math
+
+from reduced_residue_system import (ancestors_compositeness, random_rrsp,
+                                    reduced_residue_system_primorial)
+from sympy import isprime, prime
 
 
-def main():
+def there_are_enough_non_two_primes():
+
+    def num_rrsp(i):
+        return math.prod((prime(i) - 1 for i in range(1, i + 1)))
+
+    def num_rrsp_2(i):
+        return math.prod((prime(i) - 2 for i in range(2, i + 1)))
+
+    def num_prime_rrsp(i):
+        return sum(isprime(r) for r in reduced_residue_system_primorial(i))
+
+    for i in range(1, 10):
+        num_rrsp_i = num_rrsp(i)
+        num_rrsp_2_i = num_rrsp_2(i)
+        print(num_rrsp_i, num_rrsp_2_i, num_rrsp_i - num_rrsp_2_i,
+              num_prime_rrsp(i))
+
+
+def ancestors_compositeness_counts():
     depth = 80
     counts = [0 for _ in range(depth)]
     for i in range(1000):
@@ -12,5 +34,9 @@ def main():
         print(i, counts)
 
 
+def main():
+    there_are_enough_non_two_primes()
+
+
 if __name__ == '__main__':
-    main()
+    there_are_enough_non_two_primes()
